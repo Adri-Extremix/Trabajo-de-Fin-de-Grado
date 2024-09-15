2fe07012002522173d956eb2d6d0f86598acc94d
Adrian <adrianfernandezgalan@gmail.com>
2024-09-15 20:19:47 +0200

    Cambios de tamaño

--- src/front/src/App.js
+++ src/front/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./App.css"; // Importación de los estilos específicos para este componente
+import "./App.css";
 import Header from "./components/Header/Header";
 import MainContent from "./components/MainContent/MainContent";
 
