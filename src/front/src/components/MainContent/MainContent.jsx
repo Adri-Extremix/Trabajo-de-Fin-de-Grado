fced67a9426b03a7a9483eee3eafc320a30b39cf
Adrian <adrianfernandezgalan@gmail.com>
2024-09-15 19:42:06 +0200

    Terminada parte sin funcionalidad de la edición de código

--- src/front/src/components/MainContent/MainContent.jsx
+++ src/front/src/components/MainContent/MainContent.jsx
@@ -2,10 +2,25 @@ import React from "react";
 import "./MainContent.css";
 import Editor from "./Editor/Editor";
 import Terminal from "./Terminal/Terminal";
+import Button from "./Button/Button";
 function MainContent() {
+    const Compilar = () => {
+        console.log("Compilando");
+    };
+
+    const Ejecutar = () => {
+        console.log("Ejecutando");
+    };
+
     return (
         <div className="MainContent">
-            <Editor />
+            <div className="Code">
+                <Editor />
+                <div className="Buttons">
+                    <Button text="Compilar" onClick={Compilar} />
+                    <Button text="Ejecutar" onClick={Ejecutar} />
+                </div>
+            </div>
             <Terminal />
         </div>
     );
