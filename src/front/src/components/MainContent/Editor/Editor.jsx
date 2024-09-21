ef99639055488b54848d1fcf102eed1c679a3c1b
Adrian <adrianfernandezgalan@gmail.com>
2024-09-21 12:59:30 +0200

    Editor de código simplón pero funcional

--- src/front/src/components/MainContent/Editor/Editor.jsx
+++ src/front/src/components/MainContent/Editor/Editor.jsx
@@ -1,8 +1,29 @@
 import React from "react";
+import CodeMirror from "@uiw/react-codemirror";
+import { javascript } from "@codemirror/lang-javascript";
+import { cpp } from "@codemirror/lang-cpp";
+import { vscodeDark } from "@uiw/codemirror-theme-vscode";
+import { tokyoNight } from "@uiw/codemirror-theme-tokyo-night";
 import "./Editor.css";
 
 function Editor() {
-    return <div className="Editor">Editor</div>;
+    const [value, setValue] = React.useState("//Escribe aquí su código");
+    const onChange = React.useCallback((val, viewUpdate) => {
+        console.log("val:", val);
+        setValue(val);
+    }, []);
+
+    return (
+        <div className="Editor">
+            <CodeMirror
+                value={value}
+                extensions={[cpp()]}
+                onChange={onChange}
+                theme={tokyoNight}
+                className="cm-editor"
+            />
+        </div>
+    );
 }
 
 export default Editor;
